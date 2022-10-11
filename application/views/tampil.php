c65cf6135860510b6ba915d53c86be42c0def4f7
Thoms251102 <113426651+Thoms251102@users.noreply.github.com>
2022-10-11 08:31:28 -0600

    Add files via upload

---
+++ application/views/tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Latihan pertama</title>
+</head>
+<body>
+    <p>Latihan pertama saya belajar framework codeigniter</p>
+    <p>Framework codeigniter sangat mudah dipelajari</p>
+
+</body>
+</html>
